09ba5b27550104b161ab6e9fc7778b88f7c8492f
samk90 <kim.donghy@northeastern.edu>
2021-06-22 14:28:58 -0400

    adding my java files -sam

---
+++ sam-jsonparse2-maven/src/main/java/cs5500/project/Summary.java
@@ -0,0 +1,10 @@
+package cs5500.project;
+
+public class Summary {
+
+  private String activityType;
+  private String groupType;
+  private double duration;
+  private double distance;
+
+}
---
+++ sam-jsonparse2-maven/src/test/java/cs5500/project/jsonStoryLineParse.java
@@ -0,0 +1,31 @@
+package cs5500.project;
+
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import org.junit.Test;
+
+public class jsonStoryLineParse {
+
+  @Test
+  public void count() {
+    InputStream inputStream = getClass().getClassLoader()
+        .getResourceAsStream("storylineOneDay");
+    Reader reader = new InputStreamReader(inputStream);
+    JsonElement rootElement = JsonParser.parseReader(reader);
+    System.out.println(rootElement.getClass());
+    JsonArray array = rootElement.getAsJsonArray();
+    //JsonObject rootObject = rootElement.getAsJsonObject();
+    //System.out.println(array);
+
+    System.out.println(array.get(0).getAsJsonObject().get("summary").getAsJsonArray().get(0).getAsJsonObject().get("distance"));
+//    for (Entry<String, JsonElement> entry: rootObject.entrySet()) {
+//      System.out.println(entry.getValue());
+//    }
+  }
+
+}
