08b28cf69ad68b7b2ba0ed1b0804e69be8737ee4
zachasyl <85774757+zachasyl@users.noreply.github.com>
2021-07-06 10:44:07 -0400

    Update Main.java

--- DtoDao/src/Main.java
+++ DtoDao/src/Main.java
@@ -2,16 +2,12 @@
 public class Main {
     /**
      * The main function creates a ParseJson object, AllData, and calls its ParseJson method
-     * FillArrays(). This reads a JSON file and fills five arrays belonging to the ParseJson object:
-     * activities, duration, calories, distance, and date.
+     * FillObjects(). This reads a JSON file and fills objects.
      *
-     * A JDBC object then calls the TableInsertion function, taking in each ParseJson
-     * array as a parameter to fill a MYSQL database.
+     * Within ParseJson, DAO's add method is called on each filled object to add it to the database.
      */
     public static void main(String[] args) {
         ParseJSON AllData = new ParseJSON();
-        AllData.FillArrays();
-
-
+        AllData.FillObjects();
     }
 }
