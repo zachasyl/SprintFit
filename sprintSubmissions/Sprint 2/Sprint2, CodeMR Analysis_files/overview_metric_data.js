136a88264a813c037a42251c8dded366db791840
Sam D. Kim <51936705+samk901@users.noreply.github.com>
2021-07-24 05:41:34 -0400

    codemr

---
+++ sprintSubmissions/Sprint 2/Sprint2, CodeMR Analysis_files/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"MyFitnessAppwithMVC", "Number of Highly Problematic Classes":0, "Number of Entities":10, "Number of Problematic Classes":0, "Class Lines of Code":194, "Number of External Packages":12, "Number of Packages":3, "Number of External Entities":39};var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
