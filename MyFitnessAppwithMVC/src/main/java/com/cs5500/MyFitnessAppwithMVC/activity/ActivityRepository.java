82be629d85bf95c9f8a98ede85709753e8bfe641
Zachary <zachary@Zacharys-MacBook-Pro.local>
2021-07-27 23:26:59 -0400

    added sort features

--- MyFitnessAppwithMVC/src/main/java/com/cs5500/MyFitnessAppwithMVC/activity/ActivityRepository.java
+++ MyFitnessAppwithMVC/src/main/java/com/cs5500/MyFitnessAppwithMVC/activity/ActivityRepository.java
@@ -1,6 +1,10 @@
 package com.cs5500.MyFitnessAppwithMVC.activity;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * This interface extends the Spring framework's Crud Repository, which allows for generic
@@ -14,4 +18,12 @@ public interface ActivityRepository extends CrudRepository<Activity, Integer> {
    * @return (Long) number of entries matching id value
    */
   public Long countById(Integer id);
-}
+
+  /**
+   * Adding project specific queries to Spring framework's Crud repo
+   */
+  @Query(value = "SELECT c FROM Activity c WHERE c.activityName = 'Running' ORDER BY Calories DESC")
+  List<Activity> findRunning();
+  @Query(value = "SELECT c FROM Activity c ORDER BY Calories DESC")
+  List<Activity> orderCalories();
+};
