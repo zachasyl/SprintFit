56e2c200a5e8181c2bc6f22a25c06820476996fb
Sam D. Kim <51936705+samk901@users.noreply.github.com>
2021-08-07 13:19:53 -0400

    added highchart pie chart view

--- MyFitnessAppwithMVC/src/main/java/com/cs5500/MyFitnessAppwithMVC/activity/ActivityController.java
+++ MyFitnessAppwithMVC/src/main/java/com/cs5500/MyFitnessAppwithMVC/activity/ActivityController.java
@@ -8,12 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 
-//TODO Look at endpoints and follow the conventional standards for a REST API (Single "Activity"...etc)
-//TODO Create another package for the API, maybe create a package of Controllers and inside, create a
-// rest API package
-// UI "ideally" should be utilizing the API to populate pages for the customer
 /**
  * This class serves as our Controller for the Activity class. It connects and handles the UI's
  * CRUD links with an instance of the ActivityService class.
@@ -47,11 +44,47 @@ public class ActivityController {
 
   @GetMapping("/sort")
   public String sort(Model model) {
-    {
-      List<Activity> sortedList = service.sort();
-      model.addAttribute("sortedList", sortedList);
-      return "sort";
+    List<Activity> sortedList = service.sort();
+    model.addAttribute("sortedList", sortedList);
+    return "sort";
+  }
+
+  @GetMapping("/highchartview")
+  public String showHighChart(Model model) {
+    List<String> ActivityNames = new ArrayList<>();
+    ActivityNames.add("transport");
+    ActivityNames.add("walking");
+    ActivityNames.add("running");
+    ActivityNames.add("other");
+    model.addAttribute("activityNames", ActivityNames);
+
+    List<Integer> ActivityValues = new ArrayList<>();
+    Integer transport = 0;
+    Integer walking = 0;
+    Integer running = 0;
+    Integer other = 0;
+
+    List<Activity> activityList = service.listAll();
+    for (Activity a : activityList) {
+      //System.out.println(a.getDistance());
+      if (a.getActivityName().equalsIgnoreCase("transport")) {
+        transport += a.getDistance();
+      } else if (a.getActivityName().equalsIgnoreCase("walking")) {
+        walking += a.getDistance();
+      } else if (a.getActivityName().equalsIgnoreCase("running")) {
+        running += a.getDistance();
+      } else {
+        other += a.getDistance();
+      }
     }
+    ActivityValues.add(transport);
+    ActivityValues.add(walking);
+    ActivityValues.add(running);
+    ActivityValues.add(other);
+
+    model.addAttribute("activityValues", ActivityValues);
+
+    return "highchartview";
   }
 
   @PostMapping("/activity/save")
